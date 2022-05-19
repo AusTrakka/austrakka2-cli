26470f72449c77217175e1b22344c2ff775b4e32
Vi Phu <vi.phu@unimelb.edu.au>
2022-05-19 10:18:59 +1000

    AB#2487 cleaned ups, linting, and adjusting to web api contract changes.

--- austrakka/components/proforma/funcs.py
+++ austrakka/components/proforma/funcs.py
@@ -5,8 +5,7 @@ from austrakka.utils.api import get
 from austrakka.utils.misc import logger_wraps
 from austrakka.utils.output import print_table
 from austrakka.utils.paths import PROFORMA_PATH
-from austrakka.components.user.funcs import get_users
-import json
+
 
 @logger_wraps()
 def list_proformas(table_format: str):
@@ -18,47 +17,51 @@ def list_proformas(table_format: str):
         }
     )
 
-    #print("RESPONSE")
-    #print(json.dumps(response, indent=2))
-    #print("END")
-
-    result = pd.DataFrame.from_dict(response)
-
-    users = get_users()
-    users.set_index('userID', inplace=True)
+    data = response['data']
+    result = pd.DataFrame.from_dict(data)
 
     result['suggestedSpecies'] = result['suggestedSpecies'].apply(
-        lambda slist: ','.join([species['name'] for species in slist])
+        lambda slist: ','.join([species['abbrev'] for species in slist])
     )
 
-    result.drop(['columnMappings', 'proFormaVersionId', 'lastUpdatedBy', 'lastUpdated'],
-                axis='columns', inplace=True)
+    result.drop(
+        ['columnMappings', 'proFormaVersionId', 'lastUpdatedBy', 'lastUpdated'],
+        axis='columns',
+        inplace=True
+    )
 
     print_table(
         result,
         table_format,
     )
 
+
 @logger_wraps()
 def show_proformas(abbrev: str, table_format: str):
     response = call_api(
         method=get,
         path=f'{PROFORMA_PATH}/abbrev/{abbrev}',
-    )    
+    )
     data = response['data']
-    
+
     for field in ['abbreviation','name','version','description']:
         print(f'{field}: {data[field]}')
-    
-    SPECIES_FIELD = 'suggestedSpecies'
-    species = ','.join([s['name'] for s in data[SPECIES_FIELD]])
-    print(f'{SPECIES_FIELD}: {species}')
-    
+
+    species_field = 'suggestedSpecies'
+    species = ','.join([s['name'] for s in data[species_field]])
+    print(f'{species_field}: {species}')
+
     print('Pro forma fields:')
-    
+
     # Should add isActive check, but probably in endpoint
-    field_df = pd.DataFrame.from_dict(data['columnMappings'])[['metaDataColumnName','metaDataColumnPrimitiveType','isRequired']]
-    field_df.rename(columns={'metaDataColumnPrimitiveType': 'type'}, inplace=True)
+    field_df = pd.DataFrame.from_dict(data['columnMappings'])[
+        ['metaDataColumnName','metaDataColumnPrimitiveType','isRequired']]
+
+    field_df.rename(
+        columns={'metaDataColumnPrimitiveType': 'type'},
+        inplace=True
+    )
+
     field_df['type'].fillna('categorical', inplace=True)
     print_table(
             field_df,
