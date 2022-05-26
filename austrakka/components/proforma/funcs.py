6facc288e2dfec3f0a9e711cc7231eaee11cbc4f
Clare Sloggett <claresloggett@gmail.com>
2022-05-26 10:31:13 +1000

    Basic pro-forma add command

--- austrakka/components/proforma/funcs.py
+++ austrakka/components/proforma/funcs.py
@@ -1,12 +1,39 @@
 import pandas as pd
 from loguru import logger
 
+from typing import List
+
 from austrakka.utils.api import call_api
-from austrakka.utils.api import get
+from austrakka.utils.api import get, post
 from austrakka.utils.misc import logger_wraps
 from austrakka.utils.output import print_table
 from austrakka.utils.paths import PROFORMA_PATH
 
+@logger_wraps()
+def add_proforma(
+        abbrev: str, 
+        name: str, 
+        description: str, 
+        suggested_species: List[str],
+        required_columns: List[str],
+        optional_columns: List[str]):
+        
+    # TODO: check that all system fields are included (or let endpoint take care of it?)
+    columnNames = ([{"name": col, "isRequired": True} for col in required_columns] 
+                    + [{"name": col, "isRequired": False} for col in optional_columns])
+    if len(columnNames)==0:
+        raise ValueError("A pro forma must contain at least one field")
+    return call_api(
+        method=post,
+        path=PROFORMA_PATH,
+        body={
+            "abbreviation": abbrev,
+            "name": name,
+            "description": description,
+            "speciesAbbrev": suggested_species,
+            "columnNames": columnNames  
+        })
+       
 
 @logger_wraps()
 def list_proformas(table_format: str):
