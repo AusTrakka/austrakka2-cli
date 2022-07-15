54ae562fd4be255ec13e93b752cc2f462063b912
Samuel Carswell <samuelrcarswell@gmail.com>
2022-07-15 16:25:58 +1000

    AB#2944 Add `analysis definition update` command

--- austrakka/components/analysis/definition/__init__.py
+++ austrakka/components/analysis/definition/__init__.py
@@ -3,6 +3,7 @@ from typing import List
 import click
 
 from austrakka.components.analysis.definition.funcs import add_definition
+from austrakka.components.analysis.definition.funcs import update_definition
 from austrakka.utils.options import opt_species
 from austrakka.utils.options import opt_name
 from austrakka.utils.options import opt_description
@@ -20,7 +21,7 @@ def definition(ctx):
 @opt_name()
 @opt_description()
 @opt_species(multiple=True)
-@opt_is_active
+@opt_is_active()
 def definition_add(
         name: str,
         description: str,
@@ -29,3 +30,18 @@ def definition_add(
 ):
     """Add analysis definition in AusTrakka"""
     add_definition(name, description, species, is_active)
+
+
+@definition.command('update')
+@click.argument('name', type=str)
+@opt_description(required=False)
+@opt_species(required=False, multiple=True)
+@opt_is_active(is_update=True)
+def definition_update(
+        name: str,
+        description: str,
+        species: List[str],
+        is_active: bool
+):
+    """Update analysis definition in AusTrakka"""
+    update_definition(name, description, species, is_active)
--- austrakka/components/analysis/definition/funcs.py
+++ austrakka/components/analysis/definition/funcs.py
@@ -1,9 +1,14 @@
 from typing import List
 
-from austrakka.utils.api import call_api, post
+from austrakka.utils.api import call_api
+from austrakka.utils.api import post
+from austrakka.utils.api import put
 from austrakka.utils.paths import JOB_DEFINITION_PATH
+from austrakka.utils.misc import logger_wraps
+from austrakka.utils.helpers.definition import get_definition_by_name
 
 
+@logger_wraps()
 def add_definition(
         name: str,
         description: str,
@@ -25,4 +30,34 @@ def add_definition(
             "isActive": is_active,
             "unavailable": True,
         }
+    )
+
+
+@logger_wraps()
+def update_definition(
+        name: str,
+        description: str,
+        species: List[str],
+        is_active: bool
+):
+    definition = get_definition_by_name(name)
+
+    if name is not None:
+        definition['name'] = name
+
+    if description is not None:
+        definition['description'] = description
+
+    if species is not None and len(species) > 0:
+        definition["species"] = [
+            {"abbreviation": species_item} for species_item in species
+        ]
+
+    if is_active is not None:
+        definition['isActive'] = is_active
+
+    call_api(
+        method=put,
+        path=f'{JOB_DEFINITION_PATH}/{definition["jobDefinitionId"]}',
+        body=definition
     )
