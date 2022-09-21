c154f2865832e1faa8b86883adbed9e2a98065f9
Vi Phu <vi.phu@unimelb.edu.au>
2022-09-21 19:41:30 +1000

    AB#3115 Support listing of fields based on projects.

--- austrakka/components/project/__init__.py
+++ austrakka/components/project/__init__.py
@@ -2,7 +2,12 @@ import click
 
 from austrakka.utils.output import table_format_option
 from austrakka.utils.cmd_filter import hide_admin_cmds
-from .funcs import list_projects, add_project
+from .funcs import list_projects, \
+    add_project, \
+    set_field_project, \
+    unset_field_project, \
+    display_field_project
+
 from ...utils.options import *
 
 
@@ -30,3 +35,33 @@ def project_add(abbrev, name, description, org):
 def projects_list(table_format: str):
     '''List projects in AusTrakka'''
     list_projects(table_format)
+
+
+@project.command('set-field', hidden=hide_admin_cmds())
+@click.argument('abbrev', type=str)
+@opt_field_name()
+def project_set_field(abbrev, field_names):
+    '''
+    Set fields to show for this project.
+    '''
+    set_field_project(abbrev, field_names)
+
+
+@project.command('unset-field', hidden=hide_admin_cmds())
+@click.argument('abbrev', type=str)
+@opt_field_name()
+def project_unset_field(abbrev, field_names):
+    '''
+    Unset fields to show for this project.
+    '''
+    unset_field_project(abbrev, field_names)
+
+
+@project.command('display-fields', hidden=hide_admin_cmds())
+@click.argument('abbrev', type=str)
+@table_format_option()
+def project_display_field(abbrev: str, table_format: str):
+    '''
+    Unset fields to show for this project.
+    '''
+    display_field_project(abbrev, table_format)
--- austrakka/components/project/funcs.py
+++ austrakka/components/project/funcs.py
@@ -1,8 +1,13 @@
+from typing import List
+
+import pandas as pd
+
 from austrakka.utils.api import call_api
 from austrakka.utils.helpers.output import call_get_and_print_table
-from austrakka.utils.api import post
+from austrakka.utils.api import post, patch, get
 from austrakka.utils.misc import logger_wraps
 from austrakka.utils.paths import PROJECT_PATH
+from austrakka.utils.output import print_table
 
 
 @logger_wraps()
@@ -20,6 +25,59 @@ def add_project(abbrev: str, name: str, description: str, org: str):
             "isActive": True
         }
     )
+
+
+@logger_wraps()
+def set_field_project(abbrev: str, field_names: List[str]):
+
+    field_name_objs = []
+    for field_name in field_names:
+        field_name_objs.append({
+            "ColumnName": field_name
+        })
+
+    return call_api(
+        method=patch,
+        path=f'{PROJECT_PATH}/set-fields/{abbrev}',
+        body={
+            "columnNames": field_name_objs
+        }
+    )
+
+
+@logger_wraps()
+def unset_field_project(abbrev: str, field_names: List[str]):
+
+    field_name_objs = []
+    for field_name in field_names:
+        field_name_objs.append({
+            "ColumnName": field_name
+        })
+
+    return call_api(
+        method=patch,
+        path=f'{PROJECT_PATH}/unset-fields/{abbrev}',
+        body={
+            "columnNames": field_name_objs
+        }
+    )
+
+
+@logger_wraps()
+def display_field_project(abbrev: str, table_format):
+
+    response = call_api(
+        method=get,
+        path=f'{PROJECT_PATH}/display-fields/{abbrev}',
+    )
+
+    data = response['data'] if ('data' in response) else response
+    result = pd.json_normalize(data, max_level=1)
+
+    print_table(
+        result,
+        table_format,
+)
 
 
 @logger_wraps()
