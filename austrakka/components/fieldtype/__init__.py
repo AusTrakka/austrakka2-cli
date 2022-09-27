37adacbcea8e35014367e2f86138063100b7b0a6
Samuel Carswell <samuelrcarswell@gmail.com>
2022-09-27 12:58:58 +1000

    AB#3119 added `fieldtype update` command

--- austrakka/components/fieldtype/__init__.py
+++ austrakka/components/fieldtype/__init__.py
@@ -4,11 +4,14 @@ import click
 
 from austrakka.utils.cmd_filter import hide_admin_cmds
 from austrakka.utils.output import table_format_option
-from austrakka.components.fieldtype.funcs import list_fieldtypes, add_fieldtype
+from austrakka.components.fieldtype.funcs import list_fieldtypes
+from austrakka.components.fieldtype.funcs import add_fieldtype
+from austrakka.components.fieldtype.funcs import update_fieldtype
 from austrakka.components.fieldtype.value import value
 from austrakka.utils.options import opt_description
 from austrakka.utils.options import opt_name
 from austrakka.utils.options import opt_fieldtype_value
+from austrakka.utils.options import opt_is_active
 from austrakka.utils.cmd_filter import show_admin_cmds
 
 
@@ -37,3 +40,16 @@ def fieldtype_list(table_format: str):
 def fieldtype_add(name: str, description: str, values: List[str]):
     """Add a new categorical field type and its valid values"""
     add_fieldtype(name, description, valid_values=values)
+
+
+@fieldtype.command('update', hidden=hide_admin_cmds())
+@click.argument('name', type=str)
+@opt_description(required=False)
+@opt_is_active(is_update=True)
+def fieldtype_update(
+        name: str,
+        description: str,
+        is_active: bool,
+):
+    """Update a fieldtype"""
+    update_fieldtype(name, description, is_active)
