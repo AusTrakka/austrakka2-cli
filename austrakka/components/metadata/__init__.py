3a0994b1da421e00973fa8185ecf041a1863a40d
Vi Phu <vi.phu@unimelb.edu.au>
2022-12-20 17:35:10 +1100

    AB#3408 - Added command to check submissions upload without commiting.

--- austrakka/components/metadata/__init__.py
+++ austrakka/components/metadata/__init__.py
@@ -3,7 +3,7 @@ from io import BufferedReader
 import click
 
 from austrakka.utils.options import opt_proforma
-from .funcs import add_metadata
+from .funcs import add_metadata, validate_metadata
 
 
 @click.group()
@@ -19,3 +19,12 @@ def metadata(ctx):
 def submission_add(file: BufferedReader, proforma: str):
     """Upload metadata submission to AusTrakka"""
     add_metadata(file, proforma)
+
+
+@metadata.command('check')
+@click.argument('file', type=click.File('rb'))
+@opt_proforma()
+def submission_validate(file: BufferedReader, proforma: str):
+    """Check uploaded content for errors and warnings. This is a read-only
+    action. No data will modified."""
+    validate_metadata(file, proforma)
--- austrakka/components/metadata/funcs.py
+++ austrakka/components/metadata/funcs.py
@@ -6,6 +6,7 @@ from austrakka.utils.api import post
 from austrakka.utils.paths import SUBMISSION_PATH
 
 SUBMISSION_UPLOAD = 'UploadSubmissions'
+SUBMISSION_VALIDATE = 'ValidateSubmissions'
 
 
 @logger_wraps()
@@ -16,6 +17,22 @@ def add_metadata(
     call_api(
         method=post,
         path="/".join([SUBMISSION_PATH, SUBMISSION_UPLOAD]),
+        body={
+            'file': (file.name, file),
+            'proforma-abbrev': proforma_abbrev,
+        },
+        multipart=True,
+    )
+
+
+@logger_wraps()
+def validate_metadata(
+    file: BufferedReader,
+    proforma_abbrev: str
+):
+    call_api(
+        method=post,
+        path="/".join([SUBMISSION_PATH, SUBMISSION_VALIDATE]),
         body={
             'file': (file.name, file),
             'proforma-abbrev': proforma_abbrev,
