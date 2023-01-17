9318058f592dc78137157d829f35651f012192d4
Vi Phu <vi.phu@unimelb.edu.au>
2023-01-17 13:59:00 +1100

    AB#3637 Command to enable samples.

--- austrakka/components/sample/__init__.py
+++ austrakka/components/sample/__init__.py
@@ -2,7 +2,7 @@
 from io import BufferedReader
 import click
 
-from .funcs import disable_sample
+from .funcs import disable_sample, enable_sample
 
 
 @click.group()
@@ -20,9 +20,20 @@ def sample(ctx):
               type=click.STRING,
               multiple=True)
 def sample_disable(sample_id: [str]):
-    """Disable a sample. This is like a soft delete. It will be purged
-    after 30 days. To re-enable, you will have to contact an admin.
-    Once disabled, it will not be possible to upload metadata or sequences
+    """Disable a sample. This is like a soft delete. It may be purged
+    after 30 days. Once disabled, it will not be possible to upload metadata or sequences
     to the sample until it is enabled again, or until it has been purged
     from the system."""
     disable_sample(sample_id)
+
+
+@sample.command('enable')
+@click.option('-s',
+              '--sample-id',
+              help='The id of the sample to be re-enable. Multiple ids can be specified.'
+                   'Eg. -s sample1 -s sample2',
+              type=click.STRING,
+              multiple=True)
+def sample_enable(sample_id: [str]):
+    """Enable a sample. This re-enables a previously disabled sample."""
+    enable_sample(sample_id)
