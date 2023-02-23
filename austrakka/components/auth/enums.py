1f499fec8f3ac2af21d1714d08fbe34d63ecb2e8
Samuel Carswell <samuelrcarswell@gmail.com>
2023-02-23 15:46:44 +1100

    AB#3851 Remove subscription key

--- austrakka/components/auth/enums.py
+++ austrakka/components/auth/enums.py
@@ -5,4 +5,3 @@ class Auth(Enum):
     TENANT_ID = '0e5bf3cf-1ff4-46b7-9176-52c538c22a4d'
     CLIENT_ID = '16292c74-3a1d-4664-9f65-edb167bf199b'
     APP_SCOPE = 'api://df125604-3b75-46d3-a8ea-e54dc3b5e402/.default'
-    SUBSCRIPTION_KEY = 'd03bb1ce7427476e83178fc949c33e06'
