4dab729549a745c128b0de24fc7f205ced56c0fa
Clare Sloggett <claresloggett@gmail.com>
2023-03-15 15:52:31 +1100

    AB#3727 Fix org retrieval for PUT

--- austrakka/utils/helpers/orgs.py
+++ austrakka/utils/helpers/orgs.py
@@ -6,6 +6,6 @@ from ..paths import ORG_PATH
 def get_org_by_abbrev(abbrev: str):
     response = call_api(
         method=get,
-        path=f"{ORG_PATH}/abbrev/{abbrev}"
+        path=f"{ORG_PATH}/{abbrev}"
     )
     return response['data'] if ('data' in response) else response
