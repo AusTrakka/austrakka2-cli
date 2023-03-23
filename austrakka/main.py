fdf1b985cee9d0ca9f424f969406d677c7a56f15
Samuel Carswell <samuelrcarswell@gmail.com>
2023-03-24 09:44:00 +1100

    AB#4009 Verify cert

--- austrakka/main.py
+++ austrakka/main.py
@@ -33,6 +33,7 @@ from .utils.cmd_filter import show_admin_cmds
 
 CLI_PREFIX = 'AT'
 CLI_ENV = 'env'
+CTX_VERIFY_CERT = 'verify_cert'
 
 CONTEXT_SETTINGS = {"help_option_names": HELP_OPTS}
 
@@ -60,17 +61,33 @@ CONTEXT_SETTINGS = {"help_option_names": HELP_OPTS}
     show_default=True
 )
 @click.option(
+    f"--{CTX_VERIFY_CERT}",
+    show_envvar=True,
+    required=True,
+    default=True,
+    show_default=True,
+    type=bool,
+    help="Skip verification of certificate"
+)
+@click.option(
     '--log',
     show_envvar=True,
     help='Outputs logs to a temporary file',
 )
 @click.version_option(message="%(prog)s v%(version)s", version=VERSION)
 @click.pass_context
-def cli(ctx: Context, uri: str, token: str, env: str, log: str):
+def cli(
+        ctx: Context,
+        uri: str,
+        token: str,
+        env: str,
+        verify_cert: bool,
+        log: str
+):
     """
     A cli for interfacing with AusTrakka.
     """
-    ctx.creds = {'uri': uri, 'token': token}
+    ctx.creds = {'uri': uri, 'token': token, CTX_VERIFY_CERT: verify_cert}
     setup_logger(env, log)
 
 
