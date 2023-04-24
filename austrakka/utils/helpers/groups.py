bdb5325c118d7fd000fc5b65be50cb4ffff6a3b4
Samuel Carswell <samuelrcarswell@gmail.com>
2023-04-24 13:37:29 +1000

    AB#4076 PR feedback

--- austrakka/utils/helpers/groups.py
+++ austrakka/utils/helpers/groups.py
@@ -2,5 +2,5 @@ from austrakka.utils.api import api_get
 from austrakka.utils.paths import GROUP_PATH
 
 
-def get_group_by_name(abbrev: str):
-    return api_get(path=f"{GROUP_PATH}/{abbrev}")['data']
+def get_group_by_name(name: str):
+    return api_get(path=f"{GROUP_PATH}/{name}")['data']
