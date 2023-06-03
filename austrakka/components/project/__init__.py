4a158dc5d0e8503ea4fc90e0323dbc611f924667
Vi Phu <vi.phu@unimelb.edu.au>
2023-06-03 19:34:05 +1000

    AB#4218 - Commands for setting a project dashboard.

--- austrakka/components/project/__init__.py
+++ austrakka/components/project/__init__.py
@@ -5,7 +5,9 @@ from austrakka.utils.options import opt_abbrev
 from austrakka.utils.options import opt_name
 from austrakka.utils.options import opt_description
 from austrakka.utils.options import opt_organisation
-from austrakka.components.project.funcs import list_projects, add_project
+from austrakka.components.project.funcs import list_projects, \
+    add_project, \
+    set_dashboard
 
 
 @click.group()
@@ -20,11 +22,22 @@ def project(ctx):
 @opt_name(help="Project name")
 @opt_description(required=False)
 @opt_organisation(help="Requesting organisation abbreviation", required=False)
-def project_add(abbrev: str, name: str, description: str, org: str):
+@click.argument('dashboard-name', type=str)
+def project_add(abbrev: str, name: str, description: str, org: str, dashboard_name: str):
     '''
     Add a new project to AusTrakka.
     '''
-    add_project(abbrev, name, description, org)
+    add_project(abbrev, name, description, org, dashboard_name)
+
+
+@project.command('set-dashboard', hidden=hide_admin_cmds())
+@opt_name(help="name of a known dashboard")
+@click.argument('project-id', type=int)
+def dashboard_set(project_id: int, name: str):
+    '''
+    Assign a dashboard to a project.
+    '''
+    set_dashboard(project_id, name)
 
 
 @project.command('list')
--- austrakka/components/project/funcs.py
+++ austrakka/components/project/funcs.py
@@ -1,11 +1,15 @@
+from os import path
 from austrakka.utils.api import api_post
+from austrakka.utils.api import api_patch
 from austrakka.utils.helpers.output import call_get_and_print_table
 from austrakka.utils.misc import logger_wraps
 from austrakka.utils.paths import PROJECT_PATH
 
+SET_DASHBOARD = 'set-dashboard'
+
 
 @logger_wraps()
-def add_project(abbrev: str, name: str, description: str, org: str):
+def add_project(abbrev: str, name: str, description: str, org: str, dashboard_name: str):
     return api_post(
         path=PROJECT_PATH,
         data={
@@ -15,8 +19,17 @@ def add_project(abbrev: str, name: str, description: str, org: str):
             "isActive": True,
             "requestingOrg": {
                 "abbreviation": org
-            }
+            },
+            "dashboardName": dashboard_name
         }
+    )
+
+
+@logger_wraps()
+def set_dashboard(project_id: int, dashboard_name: str):
+    return api_patch(
+        path=path.join(PROJECT_PATH, SET_DASHBOARD, str(project_id)),
+        data=dashboard_name
     )
 
 
