9c230334769637d427b0c5557f134e542ee44a21
Clare Sloggett <claresloggett@gmail.com>
2023-07-20 18:33:56 +1000

    Add project update command

--- austrakka/components/project/funcs.py
+++ austrakka/components/project/funcs.py
@@ -1,8 +1,13 @@
 # pylint: disable=duplicate-code
+
+from loguru import logger
+
 from os import path
 from austrakka.utils.api import api_post
 from austrakka.utils.api import api_patch
+from austrakka.utils.api import api_put
 from austrakka.utils.helpers.output import call_get_and_print_table
+from austrakka.utils.helpers.project import get_project_by_abbrev
 from austrakka.utils.misc import logger_wraps
 from austrakka.utils.paths import PROJECT_PATH
 from austrakka.utils.paths import SET_DASHBOARD
@@ -30,6 +35,49 @@ def add_project(
         }
     )
 
+@logger_wraps()
+def update_project(
+        project_abbreviation: str,
+        abbrev: str,
+        name: str,
+        description: str,
+        is_active: bool,
+        org: str,
+        dashboard_name: str):
+    project = get_project_by_abbrev(project_abbreviation)
+    
+    # ProjectDTO fields which should go in ProjectPutDTO
+    put_project = {k: project[k] for k in [
+        'abbreviation',
+        'name',
+        'description',
+        'isActive',
+        'requestingOrg',
+        'dashboardName'
+    ]}
+    if project['requestingOrg'] is None:
+        put_project['requestingOrg'] = {'abbreviation': None}
+    
+    if abbrev is not None:
+        logger.warning(f"Updating project abbreviation from {project['abbreviation']} to {abbrev}")
+        put_project['abbreviation'] = abbrev
+    if name is not None:
+        put_project['name'] = name
+    if description is not None:
+        put_project['description'] = description
+    if is_active is not None:
+        put_project['isActive'] = is_active
+    if org is not None:
+        put_project['requestingOrg'] = {
+            "abbreviation": org
+        }
+    if dashboard_name is not None:
+        put_project['dashboardName'] = dashboard_name
+    
+    return api_put(
+        path=path.join(PROJECT_PATH, str(project_abbreviation)),
+        data=put_project
+    )
 
 @logger_wraps()
 def set_dashboard(project_id: int, dashboard_name: str):
