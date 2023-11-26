c3e5dacbe0bdb134a407f60afe385ec9490152f1
Samuel Carswell <samuelrcarswell@gmail.com>
2023-11-27 10:38:07 +1100

    AB#4936 tree enable/disable commands

--- austrakka/components/tree/__init__.py
+++ austrakka/components/tree/__init__.py
@@ -4,7 +4,10 @@ import click
 
 from .funcs import add_tree
 from .funcs import list_trees
+from .funcs import disable_tree
+from .funcs import enable_tree
 from ...utils.options import opt_analysis
+from ...utils.options import opt_tree_id
 from ...utils.output import table_format_option
 
 
@@ -29,3 +32,17 @@ def tree_add(newick: BufferedReader, analysis: str):
 def tree_list(analysis: str, out_format: str):
     '''List trees in AusTrakka'''
     list_trees(out_format, analysis)
+
+
+@tree.command('disable')
+@opt_tree_id()
+def tree_disable(tree_id: int):
+    '''Disable a tree'''
+    disable_tree(tree_id)
+
+
+@tree.command('enable')
+@opt_tree_id()
+def tree_enable(tree_id: int):
+    '''Enable a tree'''
+    enable_tree(tree_id)
