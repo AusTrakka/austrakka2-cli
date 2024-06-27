d8631eca9c9c754b1fa60242799b03a93d92c382
Vi Phu <vi.phu@unimelb.edu.au>
2024-06-27 14:59:30 +1000

    New roles commands.

--- austrakka/components/rbac/role/__init__.py
+++ austrakka/components/rbac/role/__init__.py
@@ -2,7 +2,7 @@ import click
 from austrakka.utils.cmd_filter import hide_admin_cmds
 from austrakka.utils.options import *
 from austrakka.utils.output import table_format_option
-from .funcs import get_roles
+from .funcs import get_roles, add_role, update_role
 
 
 @click.group()
@@ -20,3 +20,26 @@ def roles_get(tenant_id: str, out_format: str):
     Get the list of roles defined for a tenant.
     """
     get_roles(tenant_id, out_format)
+
+
+@role.command('add', hidden=hide_admin_cmds())
+@opt_role()
+@opt_description()
+@opt_tenant_id()
+def role_add(role: str, description: str, tenant_id: str):
+    """
+    Add a new role to the tenant.
+    """
+    add_role(role, description, tenant_id)
+
+
+@role.command('update', hidden=hide_admin_cmds())
+@opt_role()
+@opt_tenant_id()
+@opt_new_name(required=False)
+@opt_description(required=False)
+def role_update(role: str, tenant_id: str, new_name: str, description: str):
+    """
+    Update a role.
+    """
+    update_role(role, tenant_id, new_name, description)
--- austrakka/components/rbac/role/funcs.py
+++ austrakka/components/rbac/role/funcs.py
@@ -1,4 +1,4 @@
-from austrakka.utils.api import api_get
+from austrakka.utils.api import api_get, api_post, api_patch
 from austrakka.utils.misc import logger_wraps
 from austrakka.utils.output import print_dict
 
@@ -16,4 +16,42 @@ def get_roles(tenant_id: str, out_format: str):
     print_dict(
         data,
         out_format,
+    )
+
+
+@logger_wraps()
+def add_role(role: str, description: str, tenant_id: str):
+    """
+    Add a new role to a tenant.
+    """
+    payload = {
+        "name": role,
+        "description": description,
+    }
+
+    api_post(
+        path=f"v2/tenant/{tenant_id}/role",
+        data=payload,
+    )
+
+
+@logger_wraps()
+def update_role(role: str, tenant_id: str, new_name: str, description: str):
+    """
+    Update a role.
+    """
+    existing_roles = api_get(
+        path=f"v2/tenant/{tenant_id}/role",
+    )
+
+    role_obj = next((r for r in existing_roles['data'] if r['name'] == role), None)
+
+    payload = {
+        "name": new_name,
+        "description": description,
+    }
+
+    api_patch(
+        path=f"v2/tenant/{tenant_id}/role/{role_obj['roleId']}",
+        data=payload,
     )
