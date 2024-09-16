99095fbc6a2df69995808e558108a935ed3c9f21
Clare Sloggett <claresloggett@gmail.com>
2024-09-16 20:11:28 +1000

    Fix generation of proformas when the NNDSS column is included

--- austrakka/components/proforma/proforma_generation_utils.py
+++ austrakka/components/proforma/proforma_generation_utils.py
@@ -151,7 +151,7 @@ def generate_template(
     nndss_offset = 1 if nndss_column else 0
     for row,field in enumerate(fields,1):
         datadict_sheet.write_string(row, 0, field, border_format)
-        if nndss_column:
+        if nndss_column and not pd.isnull(proforma.loc[field, 'nndssFieldLabel']):
             datadict_sheet.write_string(row, 1, 
                 proforma.loc[field, 'nndssFieldLabel'],
                 border_format)
