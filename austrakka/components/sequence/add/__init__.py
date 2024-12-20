7225684d7debabcdf209b85ec42d91ef51475cf0
Samuel Carswell <samuelrcarswell@gmail.com>
2024-12-20 12:56:51 +1100

    AB#6590 Fixed help test

--- austrakka/components/sequence/add/__init__.py
+++ austrakka/components/sequence/add/__init__.py
@@ -1,4 +1,5 @@
 from io import BufferedReader
+from typing import Union
 
 import click
 
@@ -10,6 +11,13 @@ from austrakka.utils.options import opt_share
 from ..funcs import add_fasta_cns_submission
 from ..funcs import add_sequence_submission
 
+metadata_add_help_text = """
+    If no record exists for these Seq_IDs you can either provide an owner group
+    with --owner and a shared group with --share to create the samples if
+    you wish to specify no metadata other than sample ownership. Otherwise, 
+    the `austrakka metadata add` command can be used prior to sequence upload.
+"""
+
 
 @click.group()
 @click.pass_context
@@ -18,7 +26,16 @@ def add(ctx):
     ctx.context = ctx.parent.context
 
 
-@add.command(SeqType.FASTQ_ILL_PE.value)
+@add.command(SeqType.FASTQ_ILL_PE.value, help=f"""
+    Upload paired-end Illumina FASTQ data to AusTrakka
+
+    The following CSV mapping fields are required:\n
+            Seq_ID: The sample name in AusTrakka\n
+            filepath1: The local path of the first read file to be uploaded\n
+            filepath2: The local path of the second read file to be uploaded
+
+    {metadata_add_help_text}
+""")
 @click.argument('csv_file', type=click.File('rb'), required=False)
 @opt_skip_mutex_force(
     help="For each sample, skip upload if the sample has existing sequences of the same type.")
@@ -31,21 +48,21 @@ def seq_add_fastq_ill_PE(
         csv_file: BufferedReader,
         skip: bool = False,
         force: bool = False,
-        owner_group: str = None,
-        shared_group: str = None,
+        owner_group: Union[str, None] = None,
+        shared_group: Union[str, None] = None,
 ):
-    """
-    Upload paired-end Illumina FASTQ data to AusTrakka
-
-    The following CSV mapping fields are required:\n
-            Seq_ID: The sample name in AusTrakka\n
-            filepath1: The local path of the first read file to be uploaded\n
-            filepath2: The local path of the second read file to be uploaded
-    """
     add_sequence_submission(SeqType.FASTQ_ILL_PE, csv_file, skip, force, owner_group, shared_group)
 
 
-@add.command(SeqType.FASTQ_ILL_SE.value)
+@add.command(SeqType.FASTQ_ILL_SE.value, help=f"""
+    Upload single-end Illumina FASTQ data to AusTrakka
+
+    The following CSV mapping fields are required:\n
+            Seq_ID: The sample name in AusTrakka\n
+            filepath: The local path of the file to be uploaded
+
+    {metadata_add_help_text}
+""")
 @click.argument('csv_file', type=click.File('rb'))
 @opt_skip_mutex_force(
     help="For each sample, skip upload if the sample has existing sequences of the same type.")
@@ -58,20 +75,21 @@ def seq_add_fastq_ill_SE(
         csv_file: BufferedReader,
         skip: bool = False,
         force: bool = False,
-        owner_group: str = None,
-        shared_group: str = None,
+        owner_group: Union[str, None] = None,
+        shared_group: Union[str, None] = None,
 ):
-    """
-    Upload single-end Illumina FASTQ data to AusTrakka
+    add_sequence_submission(SeqType.FASTQ_ILL_SE, csv_file, skip, force, owner_group, shared_group)
+
+
+@add.command(SeqType.FASTQ_ONT.value, help=f"""
+    Upload Oxford Nanopore FASTQ data to AusTrakka
 
     The following CSV mapping fields are required:\n
             Seq_ID: The sample name in AusTrakka\n
             filepath: The local path of the file to be uploaded
-    """
-    add_sequence_submission(SeqType.FASTQ_ILL_SE, csv_file, skip, force, owner_group, shared_group)
-
-
-@add.command(SeqType.FASTQ_ONT.value)
+    
+    {metadata_add_help_text}
+""")
 @click.argument('csv_file', type=click.File('rb'))
 @opt_skip_mutex_force(
     help="For each sample, skip upload if the sample has existing sequences of the same type.")
@@ -83,20 +101,22 @@ def seq_add_fastq_ont(
         csv_file: BufferedReader,
         skip: bool = False,
         force: bool = False,
-        owner_group: str = None,
-        shared_group: str = None,
+        owner_group: Union[str, None] = None,
+        shared_group: Union[str, None] = None,
 ):
-    """
-    Upload Oxford Nanopore FASTQ data to AusTrakka
-
-    The following CSV mapping fields are required:\n
-            Seq_ID: The sample name in AusTrakka\n
-            filepath: The local path of the file to be uploaded
-    """
     add_sequence_submission(SeqType.FASTQ_ONT, csv_file, skip, force, owner_group, shared_group)
 
 
-@add.command(SeqType.FASTA_CNS.value)
+@add.command(SeqType.FASTA_CNS.value, help=f"""
+
+    Upload consensus FASTA sequences to AusTrakka.
+
+    A single FASTA file should be supplied.
+    Multiple samples can be included in the same file.
+    Contig names must correspond to known Seq_IDs.
+
+    {metadata_add_help_text}
+""")
 @click.argument('fasta_file', type=click.File('rb'))
 @opt_skip_mutex_force(
     help="For each sample, skip upload if the sample has existing sequences of the same type.")
@@ -108,26 +128,23 @@ def seq_add_fasta_cns(
         fasta_file: BufferedReader,
         skip: bool = False,
         force: bool = False,
-        owner_group: str = None,
-        shared_group: str = None,
+        owner_group: Union[str, None] = None,
+        shared_group: Union[str, None] = None,
 ):
-    """
-    Upload consensus FASTA sequences to AusTrakka.
-
-    A single FASTA file should be supplied.
-    Multiple samples can be included in the same file.
-    Contig names must correspond to known Seq_IDs.
-
-    If no record exists for these Seq_IDs you can first add them with the
-    `austrakka metadata add` command, and may use the minimal proforma if
-    you wish to specify no metadata other than sample ownership.
-    """
     # FASTA-CNS is a special case as the CLI does the work of splitting the file,
     # and there is no CSV
     add_fasta_cns_submission(fasta_file, skip, force, owner_group, shared_group)
 
 
-@add.command(SeqType.FASTA_ASM.value)
+@add.command(SeqType.FASTA_ASM.value, help=f"""
+    Upload FASTA assembly sequences to AusTrakka
+
+    The following CSV mapping fields are required:\n
+            Seq_ID: The sample name in AusTrakka\n
+            filepath: The local path of the file to be uploaded
+
+    {metadata_add_help_text}
+""")
 @click.argument('csv_file', type=click.File('rb'))
 @opt_skip_mutex_force(
     help="For each sample, skip upload if the sample has existing sequences of the same type.")
@@ -139,14 +156,7 @@ def seq_add_fasta_asm(
         csv_file: BufferedReader,
         skip: bool = False,
         force: bool = False,
-        owner_group: str = None,
-        shared_group: str = None,
+        owner_group: Union[str, None] = None,
+        shared_group: Union[str, None] = None,
 ):
-    """
-    Upload FASTA assembly sequences to AusTrakka
-
-    The following CSV mapping fields are required:\n
-            Seq_ID: The sample name in AusTrakka\n
-            filepath: The local path of the file to be uploaded
-    """
     add_sequence_submission(SeqType.FASTA_ASM, csv_file, skip, force, owner_group, shared_group)
