24223eae616fa9af23d1fe454981e4c66fd0e8f9
Samuel Carswell <samuelrcarswell@gmail.com>
2025-02-27 08:31:47 +1100

    AB#6749 Removed test code

--- austrakka/__init__.py
+++ austrakka/__init__.py
@@ -1,4 +1,4 @@
 # NOTE: do not change the name of this variable or the quote type,
 # doing so will break github actions.
-__version__ = "0.68.0-1"
+__version__ = "0.68.0"
 __prog_name__ = "AusTrakka"
