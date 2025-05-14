dcd68531935470b6823aec457bfcf1f3c35f6b7a
Vi Phu <vi.phu@unimelb.edu.au>
2025-05-14 11:04:42 +1000

    AB#6676 - Use call_get_and_print when fetching activity logs.

--- austrakka/components/activity/funcs.py
+++ austrakka/components/activity/funcs.py
@@ -1,9 +1,6 @@
-import pandas as pd
-
-from austrakka.utils.api import api_get
+from austrakka.utils.helpers.output import call_get_and_print
 from austrakka.utils.helpers.tenant import get_default_tenant_global_id
 from austrakka.utils.misc import logger_wraps
-from austrakka.utils.output import print_dataframe
 
 
 @logger_wraps()
@@ -28,15 +25,8 @@ def get_activity_by_record_type(
     Get the activity log for a specific record type.
     """
     tenant_global_id = get_default_tenant_global_id()
-    response = api_get(
-        path=f"v2/{record_type}/{record_global_id}/activitylog?"
-             f"owningTenantGlobalId={tenant_global_id}",
-    )
-
-    data = response['data'] if ('data' in response) else response
-    result = pd.DataFrame.from_dict(data)
-
-    print_dataframe(
-        result,
-        out_format,
+    call_get_and_print(
+        path=f"v2/{record_type}/{record_global_id}/activitylog",
+        out_format=out_format,
+        params={'owningTenantGlobalId': tenant_global_id},
     )
