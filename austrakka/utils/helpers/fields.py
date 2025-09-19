c47db60389fed030109b3280949d821cb17910a3
Clare Sloggett <claresloggett@gmail.com>
2025-09-19 20:51:23 +1000

    Linting fixes

--- austrakka/utils/helpers/fields.py
+++ austrakka/utils/helpers/fields.py
@@ -4,8 +4,6 @@ from austrakka.utils.paths import METADATACOLUMNS_PATH, TENANT_PATH, METADATACOL
 def get_field_by_name(name: str):
     return api_get(path=f"{METADATACOLUMNS_PATH}/name/{name}")['data']
 
-
 def get_field_by_name_v2(tenant_global_id:str, name: str):
     return api_get(
         path=f"{TENANT_PATH}/{tenant_global_id}/{METADATACOLUMN_PATH}/name/{name}")['data']
-
