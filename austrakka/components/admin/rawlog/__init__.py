9f01505ff99ffda13363b807bc1021c08a13415f
Clare Sloggett <claresloggett@gmail.com>
2026-02-05 16:19:52 +1100

    AB#7750 Add commands to list and view raw log entries

---
+++ austrakka/components/admin/rawlog/__init__.py
@@ -0,0 +1,36 @@
+import click
+
+from austrakka.utils.options import opt_global_id, opt_view_type
+from austrakka.utils.output import object_format_option, table_format_option
+from austrakka.components.admin.rawlog.funcs import show_raw_log, list_raw_logs
+
+@click.group('rlog')
+@click.pass_context
+def rawlog(ctx):
+    """Commands related to raw logs"""
+    ctx.context = ctx.parent.context
+
+@rawlog.command('show')
+@opt_global_id(help="Raw log global ID")
+@object_format_option()
+def rawlog_show(global_id: str, out_format: str):
+    '''
+    Get a single raw log by global ID.
+    '''
+    show_raw_log(global_id, out_format)
+
+@rawlog.command('list')
+@click.option('--spec', help='Type class of raw event to filter on', required=False)
+@click.option('--start', help='Start datetime to filter from (ISO 8601)', required=False)
+@click.option('--end', help='End datetime to filter to (ISO 8601)', required=False)
+@click.option('--submitter', help='Submitter global ID to filter on', required=False)
+@click.option('--allow-no-filters', is_flag=True, default=False,
+              help="Allow listing without any filters, which will return ALL raw logs")
+@table_format_option(default='json')
+@opt_view_type()
+def rawlog_list(spec: str, start: str, end: str, submitter: str,
+        allow_no_filters: bool, out_format: str, view_type: str):
+    '''
+    Get a list of raw logs entries.
+    '''
+    list_raw_logs(spec, start, end, submitter, allow_no_filters, out_format, view_type)
---
+++ austrakka/components/admin/rawlog/funcs.py
@@ -0,0 +1,43 @@
+
+from austrakka.utils.helpers.output import call_get_and_print
+from austrakka.utils.output import get_viewtype_columns
+from austrakka.utils.paths import TENANT_PATH
+
+def show_raw_log(global_id: str, out_format: str):
+    '''
+    Get a single raw log by global ID.
+    '''
+    call_get_and_print(
+        f"{TENANT_PATH}/RawLog/{global_id}",
+        out_format,
+    )
+    
+
+def list_raw_logs(spec: str, start: str, end: str, submitter: str,
+                  allow_no_filters: bool, out_format: str, view_type: str):
+    '''
+    Get a list of raw log entries.
+    '''
+    # Default tabular columns: all except data
+    compact_fields = [
+        "globalId","submitterGlobalId","clientSessionId","eventTime","eventStatus","spec",
+        "logStatus","callId"
+    ]
+    
+    params = { "allowNoFilters": str(allow_no_filters).lower() }
+    if spec is not None:
+        params["spec"] = spec
+    if start is not None:
+        params["startDateTime"] = start
+    if end is not None:
+        params["endDateTime"] = end
+    if submitter is not None:
+        params["submitterGlobalId"] = submitter
+    
+    columns = get_viewtype_columns(view_type, compact_fields, [])
+    call_get_and_print(
+        f"{TENANT_PATH}/RawLogs",
+        out_format,
+        params=params,
+        restricted_cols=columns
+    )
