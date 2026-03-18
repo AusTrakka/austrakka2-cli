4067bdb0fff1085e2aaf127f7177006b66ac0359
Samuel Carswell <samuelrcarswell@gmail.com>
2026-03-18 14:43:32 +1100

    AB#7973 Mq utility commands

--- austrakka/components/admin/message/__init__.py
+++ austrakka/components/admin/message/__init__.py
@@ -4,8 +4,14 @@ import click
 from austrakka.components.admin.message.feedback import feedback
 from austrakka.components.admin.message.dl import dead_letter
 from austrakka.components.admin.message.funcs import add_message
+from austrakka.components.admin.message.funcs import metrics_message
+from austrakka.components.admin.message.funcs import show_message
+from austrakka.components.admin.message.funcs import list_message
 from austrakka.components.admin.message.funcs import opt_queue_name
+from austrakka.components.admin.message.funcs import opt_msg_id
 from austrakka.utils.cmd_filter import show_admin_cmds
+from austrakka.utils.output import table_format_option
+from austrakka.utils.output import object_format_option
 
 
 @click.group()
@@ -28,3 +34,34 @@ def message_add(file: BufferedReader, queue_name: str):
     Add message to a queue. Accepts a json file.
     '''
     add_message(file, queue_name)
+
+
+@message.command('metrics')
+@opt_queue_name(required=False, default="")
+@table_format_option()
+def message_metrics(queue_name: str, out_format: str):
+    '''
+    List metrics for queues
+    '''
+    metrics_message(queue_name, out_format)
+
+
+@message.command('show')
+@opt_queue_name()
+@opt_msg_id()
+@object_format_option()
+def message_show(queue_name: str, msg_id: int, out_format: str):
+    '''
+    Show contents of a message
+    '''
+    show_message(queue_name, msg_id, out_format)
+
+
+@message.command('list')
+@opt_queue_name()
+@object_format_option()
+def message_list(queue_name: str, out_format: str):
+    '''
+    List messages for a queue
+    '''
+    list_message(queue_name, out_format)
