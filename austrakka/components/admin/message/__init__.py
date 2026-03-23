021bc907a5761164b4a8ac1893dea228fdfaa8bb
Samuel Carswell <samuelrcarswell@gmail.com>
2026-03-24 07:20:54 +1100

    AB#8000 Fix queue commands

--- austrakka/components/admin/message/__init__.py
+++ austrakka/components/admin/message/__init__.py
@@ -29,39 +29,39 @@ message.add_command(feedback) if show_admin_cmds() else None
 @message.command('add')
 @click.argument('file', type=click.File('r'))
 @opt_queue_name()
-def message_add(file: BufferedReader, queue_name: str):
+def message_add(file: BufferedReader, queue: str):
     '''
     Add message to a queue. Accepts a json file.
     '''
-    add_message(file, queue_name)
+    add_message(file, queue)
 
 
 @message.command('metrics')
 @opt_queue_name(required=False, default="")
 @table_format_option()
-def message_metrics(queue_name: str, out_format: str):
+def message_metrics(queue: str, out_format: str):
     '''
     List metrics for queues
     '''
-    metrics_message(queue_name, out_format)
+    metrics_message(queue, out_format)
 
 
 @message.command('show')
 @opt_queue_name()
 @opt_msg_id()
 @object_format_option()
-def message_show(queue_name: str, msg_id: int, out_format: str):
+def message_show(queue: str, msg_id: int, out_format: str):
     '''
     Show contents of a message
     '''
-    show_message(queue_name, msg_id, out_format)
+    show_message(queue, msg_id, out_format)
 
 
 @message.command('list')
 @opt_queue_name()
 @object_format_option()
-def message_list(queue_name: str, out_format: str):
+def message_list(queue: str, out_format: str):
     '''
     List messages for a queue
     '''
-    list_message(queue_name, out_format)
+    list_message(queue, out_format)
