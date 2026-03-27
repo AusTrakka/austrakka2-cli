6df651d3b082bf301f103c337708cdd543949d98
Clare Sloggett <claresloggett@gmail.com>
2026-03-27 18:24:08 +1100

    CLI commands for bulk log regeneration from raw logs

--- austrakka/components/admin/rawlog/__init__.py
+++ austrakka/components/admin/rawlog/__init__.py
@@ -2,7 +2,8 @@ import click
 
 from austrakka.utils.options import opt_identifier, opt_view_type
 from austrakka.utils.output import object_format_option, table_format_option
-from austrakka.components.admin.rawlog.funcs import show_raw_log, list_raw_logs, regenerate_raw_log
+from austrakka.components.admin.rawlog.funcs import \
+    show_raw_log, list_raw_logs, regenerate_raw_log, regenerate_raw_log_bulk
 
 
 @click.group('rlog')
@@ -36,7 +37,21 @@ def rawlog_list(spec: str, start: str, end: str, submitter: str, allow_no_filter
     '''
     list_raw_logs(spec, start, end, submitter, allow_no_filters, out_format, view_type)
 
-@rawlog.command('regenerate')
+@rawlog.command('regen')
 @opt_identifier(help="Raw log global ID")
 def rawlog_regenerate(global_id: str):
+    """Regenerate logs derived from a raw log entry"""
     regenerate_raw_log(global_id)
+
+@rawlog.command('regen-bulk')
+@click.option('--spec', help='Type class of raw event to filter on', required=False)
+@click.option('--start', help='Start datetime to filter from', required=False)
+@click.option('--end', help='End datetime to filter to', required=False)
+@click.option('--submitter', help='Submitter global ID to filter on', required=False)
+@click.option('--allow-no-filters', is_flag=True, default=False,
+              help="Allow listing without any filters, which will return ALL raw logs")
+def rawlog_bulk_regen(spec: str, start: str, end: str, submitter: str, allow_no_filters: bool):
+    '''
+    Regenerate logs derived from multiple raw log entries.
+    '''
+    regenerate_raw_log_bulk(spec, start, end, submitter, allow_no_filters)
