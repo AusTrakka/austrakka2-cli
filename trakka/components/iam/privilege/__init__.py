5908b86500c79d6ea091c2065fa0b97e340d22a3
Samuel Carswell <samuelrcarswell@gmail.com>
2026-04-30 17:19:17 +1000

    AB#8076 Linting

--- trakka/components/iam/privilege/__init__.py
+++ trakka/components/iam/privilege/__init__.py
@@ -1,3 +1,2 @@
 
-# TODO: come back to this. Pretty sure I can't use relative imports with cxfreeze
 from .subcommands import privilege_subcommands
--- trakka/components/log/__init__.py
+++ trakka/components/log/__init__.py
@@ -1,5 +1,4 @@
 
 # This component has reusable subcommands which are added to multiple commands,
 # including top-level
-# TODO: check this
 from .subcommands import log_subcommands
