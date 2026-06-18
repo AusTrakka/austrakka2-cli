d09ad9260fd3797525134ee622fe27f8366b1735
Clare Sloggett <claresloggett@gmail.com>
2026-06-18 18:25:24 +1000

    Use org metadata get

--- austrakka/components/org/metadata/__init__.py
+++ austrakka/components/org/metadata/__init__.py
@@ -6,8 +6,8 @@ from austrakka.utils.options import opt_field_name, opt_identifier
 from austrakka.utils.output import table_format_option
 from austrakka.utils.output import FORMATS
 
-from .funcs import list_metadata
-from .funcs import list_metadata_by_field
+from .funcs import get_metadata
+from .funcs import get_metadata_by_field
 
 # pylint: disable=R0801
 @click.group()
@@ -18,15 +18,15 @@ def metadata(ctx):
     ctx.context = ctx.parent.context
 
 
-@metadata.command('list')
+@metadata.command('get')
 @opt_identifier(help="Org identifier")
 @opt_field_name(
     required=False, 
     help="Fields to retrieve; if none specified, all fields will be retrieved")
 @table_format_option(FORMATS.CSV)
-def metadata_list(identifier: str, field_names: List[str], out_format: str):
+def metadata_get(identifier: str, field_names: List[str], out_format: str):
     """List metadata for an org"""
     if len(field_names)==0:
-        list_metadata(identifier, out_format)
+        get_metadata(identifier, out_format)
     else:
-        list_metadata_by_field(identifier, field_names, out_format)
+        get_metadata_by_field(identifier, field_names, out_format)
--- austrakka/components/org/metadata/funcs.py
+++ austrakka/components/org/metadata/funcs.py
@@ -7,14 +7,14 @@ from austrakka.utils.paths import ORG_V2_PATH
 
 
 @logger_wraps()
-def list_metadata(identifier: str, out_format: str):
+def get_metadata(identifier: str, out_format: str):
     call_get_and_print(
         f'{ORG_V2_PATH}/{identifier}/Metadata',
         out_format,
     )
 
 @logger_wraps()
-def list_metadata_by_field(identifier: str, field_names: List[str], out_format: str):
+def get_metadata_by_field(identifier: str, field_names: List[str], out_format: str):
     call_get_and_print(
         f'{ORG_V2_PATH}/{identifier}/Metadata/Fields',
         out_format,
