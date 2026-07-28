5293b56d5e07cbb5afa7c850d5bf36ccf5d17497
Samuel Carswell <samuelrcarswell@gmail.com>
2026-07-28 16:53:31 +0930

    AB#8343 Update object id

---
+++ trakka/components/admin/user/__init__.py
@@ -0,0 +1,19 @@
+import click
+
+from trakka.components.admin.user.funcs import update_object_id
+from trakka.utils.options import opt_identifier, opt_user_object_id
+
+@click.group('user')
+@click.pass_context
+def user(ctx):
+    """Commands related to raw logs"""
+    ctx.context = ctx.parent.context
+
+@user.command('update-object-id')
+@opt_identifier(help="User Identifier")
+@opt_user_object_id()
+def object_id_update(identifier: str, user_id: str):
+    '''
+    Update a user's Azure object ID
+    '''
+    update_object_id(identifier, user_id)
