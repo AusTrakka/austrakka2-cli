790cad22b8d779c1fc6470f812fcf71f7f405d0f
Oli Jacobs <oliver.jacobs@unimelb.edu.au>
2026-09-16 11:54:55 +1000

    AB#8415 add show_disabled options for project and organisation list commands

--- trakka/utils/options.py
+++ trakka/utils/options.py
@@ -784,7 +784,6 @@ def opt_field_and_source(**attrs: t.Any):
         **{**defaults, **attrs}
     )
 
-
 def opt_show_disabled(**attrs: t.Any):
     defaults = {
         'help': 'Shows or hides disabled entities [default: --hide-disabled]'
